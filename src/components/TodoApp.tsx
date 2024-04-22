90e875bf34c6aa71390c465e8d0924fc8bd3dd63
Zeqell <sch.r.ezequiel@gmail.com>
2024-04-22 11:27:29 -0300

    proyecto lista tareas

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import {ListaTareas} from './ListaTareas'
+
+export const TodoApp = () => {
+    const [nuevaTarea, setNuevaTarea] = useState<string>('')
+    const [listaTareas, setListaTarea] = useState<string []> ([])
+
+    const handleAddTask = () =>{
+        if(nuevaTarea.trim()==='')return
+        setListaTarea(tareaAnteriores =>[...tareaAnteriores, nuevaTarea])
+        setNuevaTarea('')
+    }
+
+    const handleBorrarTarea = (index:number)=>{
+        setListaTarea(tareas => tareas.filter((_,i)=> i !== index))
+    }
+    
+    return (
+        <div>
+            <h1>Lista de Tareas</h1>
+            <div className="flex">
+                <input 
+                type="text"
+                value={nuevaTarea}
+                onChange={(e)=>setNuevaTarea(e.target.value)}
+                placeholder="Nueva Tarea"
+                />
+                <button onClick={handleAddTask}>Agregar Tarea</button>
+            </div>
+            <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea} ></ListaTareas>
+        </div>
+    )
+}
